6e4ebb474ca4301ffc097895be8763177957c4ff
Alvin Chang <alvinga@andestech.com>
2025-05-29 10:37:54 +0800

    [Alvin] Fix v2

--- core/include/mm/core_mmu.h
+++ core/include/mm/core_mmu.h
@@ -688,6 +688,9 @@ bool arch_va2pa_helper(void *va, paddr_t *pa);
 
 vaddr_t arch_aslr_base_addr(vaddr_t start_addr, uint64_t seed,
 			    unsigned int iteration_count);
+#ifdef CFG_CORE_ASLR
+void core_mmu_relocate(size_t offs);
+#endif
 
 static inline bool core_mmu_check_end_pa(paddr_t pa, size_t len)
 {
